246970a6d3d5d1e893059976704764784a64dd8a
maruyama <root@maruyama-virtual-machine.(none)>
2013-10-16 13:57:38 +0900

    maruyama2

--- ein2/2013EIN2-LOADBALANCER-CONTEST-64BIT/maruyama.rb
+++ ein2/2013EIN2-LOADBALANCER-CONTEST-64BIT/maruyama.rb
@@ -1,9 +1,7 @@
-# 複数のスイッチはつながっていないとする
 class Packet < Controller
 
   def start
     @fdb_port = {} 
-    @fdb_swit = {}
   end
   
   def switch_ready dpid  
@@ -11,41 +9,46 @@ class Packet < Controller
   end
 
   def packet_in dpid, message 
-    puts "(switch, port): (" + dpid.to_hex + ", " + message.in_port.to_s + ")"
-    source_addr = message.macsa 
-    dest_addr = message.macda 
-    @fdb_port[source_addr] = message.in_port
-    @fdb_swit[source_addr] = dpid
-    # Forwarding DBの参照
-    out_port = @fdb_port[dest_addr] 
-    out_swit = @fdb_swit[dest_addr]
-    if !out_swit || out_swit == dpid
-      if out_port
-        flow_add(dpid, message, out_port)
-      else
-        out_port = OFPP_FLOOD
-        out_swit = dpid
-      end
-      send_packet(out_swit, message, out_port)
+    in_port = message.in_port
+    out_port = @fdb_port[message.macda] 
+    print_packet(in_port, out_port)
+    @fdb_port[message.macsa] = in_port
+    if out_port
+      flow_add(out_port, message)
+      send_packet(dpid, message)
     else
-      # 何もしない
+      flood_packet(dpid, message)
     end
   end
 
   private
 
-  def flow_add dpid, message, out_port
-    out_port = @fdb_port[message.macda] 
+  def flow_add dpid, message
     send_flow_mod_add(
       dpid,
       :match => Match.from(message, :in_port, :nw_src, :nw_dst),
-      :actions => SendOutPort.new(out_port))
+      :actions => SendOutPort.new(dpid))
   end
 
-  def send_packet dpid, message, out_port
+  def send_packet dpid, message
     send_packet_out(
       dpid,
       :packet_in => message,
-      :actions => Trema::SendOutPort.new(out_port))
+      :actions => Trema::SendOutPort.new(dpid))
+  end
+
+  def flood_packet dpid, message
+    send_packet_out(
+      dpid,
+      :packet_in => message,
+      :actions => Trema::SendOutPort.new(OFPP_FLOOD))
+  end
+
+  def print_packet in_port, out_port
+    if out_port
+      puts "(" + in_port.to_s + ", " + out_port.to_s + ")"
+    else
+      puts "(" + in_port.to_s + ", " + unknown port + ")"
+    end
   end
 end
