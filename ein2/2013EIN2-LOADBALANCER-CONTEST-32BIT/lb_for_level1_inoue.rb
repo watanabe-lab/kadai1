b9396f7f5c1e5dcd8c7c484994d75e3ea0e0a4b5
n-inoue <neco392000@gmail.com>
2013-10-22 16:51:23 +0900

    出力結果見やすくしてみました。

--- ein2/2013EIN2-LOADBALANCER-CONTEST-32BIT/lb_for_level1_inoue.rb
+++ ein2/2013EIN2-LOADBALANCER-CONTEST-32BIT/lb_for_level1_inoue.rb
@@ -3,7 +3,7 @@ require "pp"
 require "counter"
 class LoadBarancerForLevel1 < Controller
 
-  periodic_timer_event(:show_counter, 10)
+  #periodic_timer_event(:show_counter, 10)
 
   def start
     @fdb = {}   
@@ -51,45 +51,73 @@ class LoadBarancerForLevel1 < Controller
   end
 
   def handle_arp_request(dpid, message, port)
-    #puts message.in_port
-    #puts message.arp_sha
-    #puts message.arp_spa.to_s
-    #puts message.arp_tha
-    #puts message.arp_tpa.to_s
-    if port
-      flow_mod_add dpid, message, port
-      packet_out dpid, message, port
-    else
-      packet_flood dpid, message
-    end
+    puts ""
+    puts "ARP Requestを受信。"
+    puts "送信元ポート番号：" +  message.in_port.to_s
+    puts "送信元MACアドレス：" + message.arp_sha.to_s
+    puts "送信元IPアドレス：" + message.arp_spa.to_s
+    puts "宛先MACアドレス：" + message.arp_tha.to_s
+    puts "宛先IPアドレス：" + message.arp_tpa.to_s
+    #if port
+    #  flow_mod_add dpid, message, port
+    #  packet_out dpid, message, port
+    #else
+      packet_flood_to_super dpid, message
+    #end
   end
 
   def handle_arp_reply(dpid, message, port)
-    #puts message.in_port
-    #puts message.arp_sha
-    #puts message.arp_spa.to_s
-    #puts message.arp_tha
-    #puts message.arp_tpa.to_s
-    if port
-      flow_mod_add dpid, message, port
-      packet_out dpid, message, port
-    else
-      packet_flood_to_client dpid, message
-    end
+    puts ""
+    puts "ARP Replyを受信。"
+    puts "送信元ポート番号：" + message.in_port.to_s
+    puts "送信元MACアドレス：" + message.arp_sha.to_s
+    puts "送信元IPアドレス：" + message.arp_spa.to_s
+    puts "宛先MACアドレス：" + message.arp_tha.to_s
+    puts "宛先IPアドレス：" + message.arp_tpa.to_s
+    #if port
+    #  flow_mod_add dpid, message, port
+    #  packet_out dpid, message, port
+    #else
+      packet_flood dpid, message
+    #end
   end
 
   def handle_ipv4(dpid, message, port)
-    #puts message.ipv4_saddr.to_s
-    #puts message.ipv4_daddr.to_s
-    if port
-      flow_mod_add dpid, message, port
-      packet_out dpid, message, port
+    puts ""
+    puts "IPパケットを受信。"
+    puts "送信元IPアドレス：" + message.ipv4_saddr.to_s
+    puts "宛先IPアドレス：" + message.ipv4_daddr.to_s
+    if message.ipv4_daddr.to_s == "192.168.0.250"
+      if port
+        flow_mod_add dpid, message, port
+        packet_out dpid, message, port
+      else
+        packet_flood_to_super dpid,message
+      end
     else
-      packet_flood dpid, message
+      if port
+        flow_mod_add dpid, message, port
+        packet_out dpid, message, port
+      else
+        packet_flood dpid, message
+      end
     end
   end
 
   def flow_mod_add(dpid, message, port)
+    puts "フローテーブル書き込み。"
+    puts "送信元IPアドレス：" + message.ipv4_saddr.to_s
+    puts "宛先IPアドレス：" + message.ipv4_daddr.to_s
+    puts "宛先ポート番号：" + port.to_s
+    send_flow_mod_add(
+                      dpid,
+                      :hard_timeout => 10,
+                      :match => ExactMatch.from( message ),
+                      :actions => Trema::SendOutPort.new(port)
+                      )
+  end
+
+  def flow_mod_add_to_super(dpid, message, port)
     send_flow_mod_add(
                       dpid,
                       :hard_timeout => 10,
@@ -107,13 +135,24 @@ class LoadBarancerForLevel1 < Controller
   end
 
   def packet_flood(dpid, message)
-    #output_sent_packet(message)
+    output_sent_packet(message)
     packet_out(dpid, message, OFPP_FLOOD)
   end 
 
+  def packet_flood_to_super(dpid, message)
+    action = create_action_to_super(OFPP_FLOOD)
+    puts "宛先を192.168.0.255に書き換え！"
+    output_sent_packet(message)
+    send_packet_out(
+                    dpid,
+                    :packet_in => message,
+                    :actions => action
+                    )
+  end
+
   def create_action_to_super(port)
     [
-     Trema::SetIpDstAddr.new("192.168.0.252"),
+     Trema::SetIpDstAddr.new("192.168.0.255"),
      Trema::SendOutPort.new(port)
     ]
   end
