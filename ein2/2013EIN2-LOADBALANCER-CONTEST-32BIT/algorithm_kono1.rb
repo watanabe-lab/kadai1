0ad17a9033f1b8d15c1b98f13af2f5da24c5f922
shiorry <kouno.shiori@ist.osaka-u.ac.jp>
2013-10-25 09:39:44 +0900

    algorithm...rbの中に説明文を追記しました

--- ein2/2013EIN2-LOADBALANCER-CONTEST-32BIT/algorithm_kono1.rb
+++ ein2/2013EIN2-LOADBALANCER-CONTEST-32BIT/algorithm_kono1.rb
@@ -1,24 +1,32 @@
 ﻿# 実装のためのパーツとなるメソッドです。
-# それぞれ適切な位置で呼び出してください。
-
+# 1. Controlleの実装クラス内でメソッドを定義
+# 2. 初期化処理中に initializeServers を呼ぶ
+# 3. 送信先サーバを選ぶ処理として selectNextServer を呼ぶ
+# 4. 変数 @next_superserver に送信先サーバのID(IPアドレスの末尾,整数値)が入るのでそこへ送る
+# 5. ACK受信処理で checkAck(server_id) を呼ぶ
+#    server_id はACKを送ったサーバのID(IPアドレスの末尾,整数値)
+# 関数の内部処理が変わっても、呼出し手順は変わらないと思います。
+# 
+# 
 
   # 変数初期化  by河野
   def initializeServers
     @server_totalNumber = 5  # サーバの総数。要設定
+    @next_superserver = 0    # 次の送り先となるサーバID。IPアドレスの末尾
     @target_defserver = 0    # デフォルトの送り先となるサーバID
     @server_waiting_packet = Array.new(@server_totalNumber, 0)  # ACK待ちパケット数を記録
   end
 
 
   # 送信先を選ぶ処理  by河野
-  # 返り値 : 送信先サーバのID。IPアドレスの末尾
-  def selectServer
+  # @next_superserver が更新されるので、パケット受信ごとに1回だけ実行する
+  def selectNextServer
     i = @target_defserver
     while @server_waiting_packet[i] >= 5
       # 数値はACK待ちパケット数。要調整
       i = (i + 1) % @server_totalNumber
     end
-    dest_server = i
+    @next_superserver = i
     @server_waiting_packet[i] += 1
     # 復活判定
     if (i - @target_defserver + @server_totalNumber) % @server_totalNumber >= 2
@@ -26,7 +34,6 @@
       @server_waiting_packet[@target_defserver] = 0
       @target_defserver = (@target_defserver + 1) % @server_totalNumber
     end
-    return dest_server
   end
 
 
